823bf950b8edec59101ec6b97c1d9b8c6f59c8ef
TheWalkingFridge <martin.dimi97@gmail.com>
2020-05-23 18:57:19 +0100

    Updated to change on toggle

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os
 from pynput import keyboard
 
+HEADPHONES = 0
+SPEAKERS = 1
+
+currentDevice = HEADPHONES
+
 def activateHeadphones():
     os.system('nircmd setdefaultsounddevice "Speakers" 1')
 
 def activateSpeakers():
     os.system('nircmd setdefaultsounddevice "C27JG5x" 1')
 
+def getCurrentOutputDevice():
+    os.system('nircmd Get-DefaultAudioDevice')
+
 def on_press(key):
-    if key == keyboard.Key.scroll_lock:
-        activateHeadphones()
-    elif key == keyboard.Key.pause:
-        activateSpeakers()
+    global currentDevice, HEADPHONES, SPEAKERS
+    if key == keyboard.Key.pause:
+        if(currentDevice == HEADPHONES):
+            activateSpeakers()
+            currentDevice = SPEAKERS
+        else:
+            activateHeadphones()
+            currentDevice = HEADPHONES
 
 with keyboard.Listener(on_press=on_press) as Listener:
     Listener.join()
+
